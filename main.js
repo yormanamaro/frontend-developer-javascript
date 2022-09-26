d18782f26d0e42aff21be3128164fc40cbb27f35
Yorman Amaro <amarocarrero24@gmail.com>
2022-09-26 19:56:26 -0300

    Integration of shopping cart to html

--- main.js
+++ main.js
@@ -1,15 +1,42 @@
 const menuEmail = document.querySelector('.navbar-email');
 const deskTopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
+
 
 menuEmail.addEventListener('click', toogleDesktopMenu);
 menuHamIcon.addEventListener('click', toogleMobileMenu);
+menuCarritoIcon.addEventListener('click', toogleCarritoAside );
+
 
 function toogleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+      aside.classList.add('inactive');
+    }
+
     deskTopMenu.classList.toggle('inactive');
 }
 
 function toogleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+      aside.classList.add('inactive'); 
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+
+function toogleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+  
+    if (!isMobileMenuClosed) {
+      mobileMenu.classList.add('inactive'); 
+    }
+
+    aside.classList.toggle('inactive');
 }
