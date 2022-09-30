d6dd79bf4e6e16ce0f5e3db8249ce6c7c3598c62
Yorman Amaro <amarocarrero24@gmail.com>
2022-09-30 17:11:44 -0300

    Integration detail of product in the main

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuHamIcon = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 
 menuEmail.addEventListener('click', toogleDesktopMenu);
@@ -60,21 +61,10 @@ producList.push({
   price: 620,
   image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'  
 });
-producList.push({
-  name: 'Teclado',
-  price: 180,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'  
-});
-producList.push({
-  name: 'Mouse',
-  price: 100,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'  
-});
-
 
 /*<div class="product-card">
 <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
-<div class="product-info">
+<div class="poduct-info">
   <div>
     <p>$120,00</p>
     <p>Bike</p>
@@ -85,10 +75,92 @@ producList.push({
 </div>
 </div>*/
 
-for (product of producList) {
-  document.createElement()
+function renderProduct(arr) {
+  for (product of arr) {
+    //
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card')
+  
+    //product= {name, price, image} -> product.image
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image);
+  
+    //
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info')
+    
+    //
+    const productInfoDiv = document.createElement('div');
+  
+    
+    const productPrice = document.createElement('p');
+    productPrice.innerHTML = '$' + product.price;
+    const productName = document.createElement('p');
+    productName.innerHTML = product.name;
+  
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+  
+    //
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+  
+    productInfoFigure.appendChild(productImgCart);
+  
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+  
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+  
+    cardsContainer.appendChild(productCard);
+  }
+  
+  
+  // otros productos 
+  for (product of producList) {
+    //
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card')
+  
+    //product= {name, price, image} -> product.image
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image);
+  
+    //
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info')
+    
+    //
+    const productInfoDiv = document.createElement('div');
+  
+    
+    const productPrice = document.createElement('p');
+    productPrice.innerHTML = '$' + product.price;
+    const productName = document.createElement('p');
+    productName.innerHTML = product.name;
+  
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+  
+    //
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+  
+    productInfoFigure.appendChild(productImgCart);
+  
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+  
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+  
+    cardsContainer.appendChild(productCard);
+  }
 }
 
-
+renderProduct(producList);
 
 
